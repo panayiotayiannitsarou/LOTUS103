7d13d51d7851117418bebb764e94eb800b8b07af
panayiotayiannitsarou <panayiotaconstantinou818@gmail.com>
2025-07-21 20:59:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from step1_senarios import generate_step1_scenarios
 from step2 import step2_katanomi_zoiroi
 from step3 import step3_katanomi_idiaiterotites
 from step4 import step4_katanomi_filikes_sxeseis
-from step5 import step5_omadopoihsh_katigories, step5_katanomi_omadon_se_tmimata
+df = step5_filikoi_omades(df, num_classes)
 from step6 import step6_ypolipoi_xwris_filies
 from step7 import step7_final_check_and_fix
 from helpers.evaluation import evaluate_scenario
